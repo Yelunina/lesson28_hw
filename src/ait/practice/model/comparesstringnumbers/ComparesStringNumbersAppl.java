32ac3750d88b1b7d1386f48b3cafa777002b91d6
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-10-13 20:45:22 +0200

    init commit

---
+++ src/ait/practice/model/comparesstringnumbers/ComparesStringNumbersAppl.java
@@ -0,0 +1,4 @@
+package ait.practice.model.comparesstringnumbers;
+
+public class ComparesStringNumbersAppl {
+}
---
+++ src/ait/practice/model/palindrome/PalindromeAppl.java
@@ -0,0 +1,4 @@
+package ait.practice.model.palindrome;
+
+public class PalindromeAppl {
+}
